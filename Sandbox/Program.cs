4a82cd3ee0619df376c144b6510ad3e70f5c82ed
JoeGuillory <joeguillory15@gmail.com>
2024-10-09 16:20:35 -0500

    Inital updload

---
+++ Sandbox/Program.cs
@@ -0,0 +1,47 @@
+﻿using MathLibrary;
+using Raylib_cs;
+using System.Numerics;
+namespace Sandbox
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Raylib.InitWindow(Raylib.GetScreenWidth(), Raylib.GetScreenHeight(), "Hello World");
+            Vector2 deltaPosition = new Vector2(0, 0);
+            Vector2 position = new Vector2();
+            Raylib.SetTargetFPS(144);
+
+            while (!Raylib.WindowShouldClose())
+            {
+                int speed = 1000;
+                Raylib.BeginDrawing();
+                Raylib.ClearBackground(Color.Black);
+                Raylib.DrawRectangle((int)position.X, (int)position.Y, 80, 80, Color.SkyBlue);
+                
+                
+                
+                int up = Raylib.IsKeyDown(KeyboardKey.W);
+                int down = Raylib.IsKeyDown(KeyboardKey.S);
+                int left = Raylib.IsKeyDown(KeyboardKey.A);
+                int right = Raylib.IsKeyDown(KeyboardKey.D);
+
+                deltaPosition.Y -= up;
+                deltaPosition.Y += down;
+                deltaPosition.X -= left;
+                deltaPosition.X += right;
+ 
+                position.X = deltaPosition.X * Raylib.GetFrameTime() *  speed;
+                position.Y = deltaPosition.Y * Raylib.GetFrameTime() *  speed;
+
+                
+
+
+
+                Raylib.EndDrawing();
+            }
+
+            Raylib.CloseWindow();
+        }
+    }
+}

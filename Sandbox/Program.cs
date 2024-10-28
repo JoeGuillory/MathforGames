9497a36fa357b674a330ef6935893a718855c844
JoeGuillory <joeguillory15@gmail.com>
2024-10-28 17:00:36 -0500

    Update Program.cs

--- Sandbox/Program.cs
+++ Sandbox/Program.cs
@@ -18,9 +18,9 @@ namespace Sandbox
             t1.LocalScale = new Vector2(100, 100);
             Vector2 offset = new Vector2(t1.LocalScale.x / 2, t1.LocalScale.y / 2);
             t1.LocalPosition = new Vector2((Raylib.GetScreenWidth() * 0.33f) - offset.x, (Raylib.GetScreenHeight() * 0.33f) - offset.y);
-           
 
 
+            Transform2D t2 = new Transform2D(a);
             
 
             while (!Raylib.WindowShouldClose())
@@ -32,6 +32,7 @@ namespace Sandbox
                 Rectangle bob = new Rectangle(t1.GlobalPosition + offset,t1.GlobalScale);
                 t1.Translate(t1.Forward * 50 * Raylib.GetFrameTime());
                 t1.Rotate(1f * Raylib.GetFrameTime());
+
                 //Draw t1
                 Raylib.DrawRectanglePro(bob, new Vector2(0,0) + offset, -t1.GlobalRotationAngle * (180/(float)Math.PI), Color.Blue);
                 Raylib.DrawLineV(t1.GlobalPosition + offset, t1.GlobalPosition + offset + (t1.Forward * 100), Color.Red);

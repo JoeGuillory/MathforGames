3b65321b3c792b7f5f61866d4c72e235ce665bd7
JoeGuillory <joeguillory15@gmail.com>
2024-10-30 15:15:13 -0500

    Update Program.cs

--- Sandbox/Program.cs
+++ Sandbox/Program.cs
@@ -11,17 +11,18 @@ namespace Sandbox
         {
             Raylib.InitWindow(800, 480, "Hello World");
             Raylib.SetTargetFPS(60);
+            float ScaleMultiplyer = 100;
 
             Actor a = new Actor();
 
             Transform2D t1 = new Transform2D(a);
-            t1.LocalScale = new Vector2(100, 100);
-            Vector2 offset = new Vector2(t1.LocalScale.x / 2, t1.LocalScale.y / 2);
+            t1.LocalScale = new Vector2(1, 1);
+            
+            Vector2 offset = new Vector2((t1.LocalScale.x * ScaleMultiplyer) / 2, (t1.LocalScale.y * ScaleMultiplyer) / 2);
             t1.LocalPosition = new Vector2((Raylib.GetScreenWidth() * 0.33f) - offset.x, (Raylib.GetScreenHeight() * 0.33f) - offset.y);
 
 
-            Transform2D t2 = new Transform2D(a);
-            
+         
 
             while (!Raylib.WindowShouldClose())
             {
@@ -29,7 +30,8 @@ namespace Sandbox
                 Raylib.BeginDrawing();
                 Raylib.ClearBackground(Color.White);
 
-                Rectangle bob = new Rectangle(t1.GlobalPosition + offset,t1.GlobalScale);
+                Rectangle bob = new Rectangle(t1.GlobalPosition + offset,t1.GlobalScale * ScaleMultiplyer);
+                
                 t1.Translate(t1.Forward * 50 * Raylib.GetFrameTime());
                 t1.Rotate(1f * Raylib.GetFrameTime());
 
@@ -37,6 +39,7 @@ namespace Sandbox
                 Raylib.DrawRectanglePro(bob, new Vector2(0,0) + offset, -t1.GlobalRotationAngle * (180/(float)Math.PI), Color.Blue);
                 Raylib.DrawLineV(t1.GlobalPosition + offset, t1.GlobalPosition + offset + (t1.Forward * 100), Color.Red);
 
+               
                 Raylib.EndDrawing();
             }
 
